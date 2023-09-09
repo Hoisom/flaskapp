518676715e4dee6aa4f84e64fdc95ef4cc2814d9
hoisom <hoison0725@gmail.com>
2023-09-10 02:35:00 +1000

    Starting to create app

--- flaskr/__index__.py
+++ flaskr/__index__.py
@@ -1,17 +1,28 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app(text_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = "dev",
+        DATABASE = os.path.join(app.instance_path, flaskr.sqlite)
+    )
 
-@app.route('/')
-def index():
-    return render_template('home.html')
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
 
-@app.route('/images')
-def images():
-    return render_template('images.html')
+    @app.route('/')
+    def index():
+        return render_template('home.html')
 
-@app.route('/todo')
-def todo():
-    return render_template('todo.html')
+    @app.route('/images')
+    def images():
+        return render_template('images.html')
 
-app.run(debug=True)
+    @app.route('/todo')
+    def todo():
+        return render_template('todo.html')
+
+    return app
