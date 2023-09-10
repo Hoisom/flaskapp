1dda704fc100c0cce5c796428d884001f5ecf6b2
hoisom <hoison0725@gmail.com>
2023-09-10 10:12:29 +1000

    Basic flask structure

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,17 +1,22 @@
 import os
 from flask import Flask, render_template
 
-def create_app(text_config=None):
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY = "dev",
-        DATABASE = os.path.join(app.instance_path, flaskr.sqlite)
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
     )
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route('/')
     def index():
